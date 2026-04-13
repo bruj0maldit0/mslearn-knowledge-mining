d96707dabe6f3abf5581a88d69bf675124cae177
Irving Diaz <irvingdiazflores@gmail.com>
2026-04-14 00:26:36 +0200

    Modify Azure storage configuration in UploadDocs.cmd
    Updated Azure storage account details and subscription ID.

--- Labfiles/01-azure-search/UploadDocs.cmd
+++ Labfiles/01-azure-search/UploadDocs.cmd
@@ -2,10 +2,12 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set values for your storage account
-set subscription_id=YOUR_SUBSCRIPTION_ID
-set azure_storage_account=YOUR_AZURE_STORAGE_ACCOUNT_NAME
-set azure_storage_key=YOUR_AZURE_STORAGE_KEY
-
+::set subscription_id=YOUR_SUBSCRIPTION_ID
+::set azure_storage_account=YOUR_AZURE_STORAGE_ACCOUNT_NAME
+::set azure_storage_key=YOUR_AZURE_STORAGE_KEY
+set subscription_id=MOC Subscription-lod52420743
+set azure_storage_account=storageaisearchmx 
+set azure_storage_key=74IxeNggzwviBjbJPgqKI7PdBR11aykhVtjIZC5bh1q010kec71PRtpSn69hPCtSBYrNZMFZOnKV+PEPEpecas
 
 echo Creating container...
 call az storage container create --account-name !azure_storage_account! --subscription !subscription_id! --name margies --auth-mode key --account-key !azure_storage_key! --output none
